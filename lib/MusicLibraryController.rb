5dc29528962802dba5ef21c111b430b25cc51bb2
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-14 18:30:54 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -40,41 +40,41 @@ class MusicLibraryController
       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
-  
+
   def list_artists
       Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
         puts "#{i+1}. #{a.name}"
       end
     end
-  
+
     def list_genres
       Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |g, i|
         puts "#{i+1}. #{g.name}"
       end
     end
-  
+
     def list_songs_by_artist
       puts "Please enter the name of an artist:"
       input = gets.strip
-  
+
       if artist = Artist.find_by_name(input)
         artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
           puts "#{i+1}. #{s.name} - #{s.genre.name}"
         end
       end
     end
-  
+
     def list_songs_by_genre
       puts "Please enter the name of a genre:"
       input = gets.strip
-  
+
       if genre = Genre.find_by_name(input)
         genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
           puts "#{i+1}. #{s.artist.name} - #{s.name}"
         end
       end
     end
-  
+
     def play_song
       puts "Which song number would you like to play?"
       input = gets.strip.to_i
@@ -84,5 +84,5 @@ class MusicLibraryController
         puts "Playing #{song.name} by #{song.artist.name}"
       end
     end
-  
+
   end
