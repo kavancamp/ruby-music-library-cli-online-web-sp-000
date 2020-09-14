88f29b599302d239ef13c89939553710220fce36
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-14 18:12:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ end
  def self.find_by_name(name)
   @@all.detect{|song| song.name == name}
  end
- 
+
  def self.find_or_create_by_name(name)
       self.find_by_name(name) || self.create(name)
   end
