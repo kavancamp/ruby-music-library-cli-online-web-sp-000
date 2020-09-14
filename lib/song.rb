8f9e00b491eeada07f291fd92459cef6c3ea2c11
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-14 18:12:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 
 class Song
  extend Concerns::Findable
-attr_accessor :name
-attr_reader :artist, :genre
+ attr_accessor :name
+ attr_reader :artist, :genre
 
-@@all = []
+ @@all = []
 
 def initialize(name, artist = nil, genre = nil)
     @name = name
@@ -43,9 +43,21 @@ end
  def self.find_by_name(name)
   @@all.detect{|song| song.name == name}
  end
+ 
  def self.find_or_create_by_name(name)
       self.find_by_name(name) || self.create(name)
   end
 
+  def self.new_from_filename(filename)
+    array = filename.split(" - ")
+
+    song_name = array[1]
+    artist_name = array[0]
+    genre_name = array[2].split(".mp3").join
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+  end
 
 end
